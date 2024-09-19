a71b31b231838e14831807c5bef4406cd8736e5d
programmernb-ctrl <programmer.nb@protonmail.com>
2024-09-19 13:03:15 +0200

    added lang and item class

--- src/main/java/de/thirst/reworkedmod/ReworkedMod.java
+++ src/main/java/de/thirst/reworkedmod/ReworkedMod.java
@@ -1,5 +1,6 @@
 package de.thirst.reworkedmod;
 
+import de.thirst.reworkedmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class ReworkedMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-	}
+		ModItems.registerModItems();
+    }
 }
