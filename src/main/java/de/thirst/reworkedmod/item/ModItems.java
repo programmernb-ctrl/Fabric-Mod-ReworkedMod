b14fb074b0066d2e498e6c3280c88244de5339c8
programmernb-ctrl <programmer.nb@protonmail.com>
2024-09-19 13:07:23 +0200

    added lang and item class

---
+++ src/main/java/de/thirst/reworkedmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package de.thirst.reworkedmod.item;
+
+import de.thirst.reworkedmod.ReworkedMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item BLUE_GARNET = registerItem("blue_garnet", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(ReworkedMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        ReworkedMod.LOGGER.info("Registering Mod Items for " + ReworkedMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(BLUE_GARNET);
+        });
+    }
+}
