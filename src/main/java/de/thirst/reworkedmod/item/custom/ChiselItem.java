9ecffda325f466f98d2f3b69f1a66993fa293fc5
programmernb-ctrl <programmer.nb@protonmail.com>
2024-09-20 17:29:12 +0200

    added tooltip keys for ChiselItem translations

--- src/main/java/de/thirst/reworkedmod/item/custom/ChiselItem.java
+++ src/main/java/de/thirst/reworkedmod/item/custom/ChiselItem.java
@@ -2,16 +2,21 @@ package de.thirst.reworkedmod.item.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.world.World;
 
+import java.util.List;
 import java.util.Map;
 
 public class ChiselItem extends Item {
@@ -43,5 +48,16 @@ public class ChiselItem extends Item {
         }
 
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        if (Screen.hasShiftDown()) {
+            tooltip.add(Text.translatable("tooltip.reworkedmod.chisel.shift_down"));
+        } else {
+            tooltip.add(Text.translatable("tooltip.reworkedmod.chisel"));
+        }
+
+        super.appendTooltip(stack, context, tooltip, type);
     }
 }
